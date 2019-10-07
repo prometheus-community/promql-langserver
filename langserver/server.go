645cbd57f0c8e96ac2e8a5db0fce14cfd007d33e
Tobias Guggenmos <tguggenm@redhat.com>
2019-10-07 11:29:14 +0200

    Add stubs for Server interface.

--- langserver/server.go
+++ langserver/server.go
@@ -2,9 +2,14 @@ package langserver
 
 import (
 	"github.com/slrtbtfs/go-tools-vendored/jsonrpc2"
+	"github.com/slrtbtfs/go-tools-vendored/lsp/protocol"
 )
 
 type Server struct {
 	Conn   *jsonrpc2.Conn
 	client protocol.client
+}
+
+func notImplemented(method string) *jsonrpc2.Error {
+	return jsonrpc2.NewErrorf("method %q no yet implemented", method)
 }
