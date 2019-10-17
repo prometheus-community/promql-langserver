82aa8e8fb0f616293d881110e84e42eaeef87858
Tobias Guggenmos <tguggenm@redhat.com>
2019-10-17 19:12:28 +0200

    WIP: Function documentation on hover

--- langserver/hover.go
+++ langserver/hover.go
@@ -1,8 +1,10 @@
 package langserver
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/slrtbtfs/go-tools-vendored/lsp/protocol"
 	"github.com/slrtbtfs/prometheus/promql"
@@ -24,21 +26,54 @@ func (s *Server) Hover(_ context.Context, params *protocol.HoverParams) (*protoc
 	}
 	node := getSmallestSourroundingNode(doc.compileResult.ast, pos)
 
-	expr, ok := node.(promql.Expr)
+	markdown := nodeToDocMarkdown(node)
 
-	if ok {
+	if markdown != "" {
 		return &protocol.Hover{
 			Contents: protocol.MarkupContent{
 				Kind:  "markdown",
-				Value: fmt.Sprintf("Type: %v", expr.Type()),
+				Value: markdown,
 			},
 		}, nil
 	} else {
-		return &protocol.Hover{
-			Contents: protocol.MarkupContent{
-				Kind:  "markdown",
-				Value: fmt.Sprintf("Compile Error"),
-			},
-		}, nil
+		return nil, nil
 	}
+}
+
+func nodeToDocMarkdown(node promql.Node) string {
+	var ret bytes.Buffer
+	expr, ok := node.(promql.Expr)
+	if ok {
+		_, err := ret.WriteString(fmt.Sprintf("Type: %v\n", expr.Type()))
+		if err != nil {
+			return ""
+		}
+	}
+
+	var call *promql.Call
+	call, ok = node.(*promql.Call)
+	if ok {
+		doc := funcDocStrings(strings.ToLower(call.Func.Name))
+		_, err := ret.WriteString(doc)
+		if err != nil {
+			return ""
+		}
+	}
+	return string(ret.Bytes())
+}
+
+func funcDocStrings(name string) string {
+	// TODO
+	/*
+		matcher = regexp.MustCompile("^## `"+name+"(?m).*?^##")
+		file :=
+		documentation = string(ioutil.Read)
+	*/
+	//stub
+	if name == "label_replace" {
+		return "## `label_replace()`\n\nFor each timeseries in `v`, `label_replace(v instant-vector, dst_label string,\nreplacement string, src_label string, regex string)` matches the regular\nexpression `regex` against the label `src_label`.  If it matches, then the\ntimeseries is returned with the label `dst_label` replaced by the expansion of\n`replacement`. `$1` is replaced with the first matching subgroup, `$2` with the\nsecond etc. If the regular expression doesn't match then the timeseries is\nreturned unchanged.\n\nThis example will return a vector with each time series having a `foo`\nlabel with the value `a` added to it:\n\n```\nlabel_replace(up{job=\"api-server\",service=\"a:c\"}, \"foo\", \"$1\", \"service\", \"(.*):.*\")\n```\n"
+	} else {
+		return ""
+	}
+
 }
