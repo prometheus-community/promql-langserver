ea8055483cee523f9c5699fdb68add778cd164c2
Tobias Guggenmos <tguggenm@redhat.com>
2019-11-14 15:01:16 +0100

    Add context expiration test for Document Methods as described in #53
    Signed-off-by: Tobias Guggenmos <tguggenm@redhat.com>

---
+++ langserver/cache/document_test.go
@@ -0,0 +1,58 @@
+// Copyright 2019 Tobias Guggenmos
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cache
+
+import (
+	"context"
+	"go/token"
+	"testing"
+)
+
+// Call the (* Document) Functions with an expired context. Expected behaviour is that all
+// of these calls return an error
+func TestDocumentContext(t *testing.T) {
+	d := &Document{}
+
+	expired, cancel := context.WithCancel(context.Background())
+
+	cancel()
+
+	if _, err := d.GetContent(expired); err == nil {
+		panic("Expected GetContent to fail with expired context")
+	}
+
+	if _, err := d.GetSubstring(expired, token.NoPos, token.NoPos); err == nil {
+		panic("Expected GetSubstring to fail with expired context")
+	}
+
+	if _, err := d.GetQueries(expired); err == nil {
+		panic("Expected GetQueries to fail with expired context")
+	}
+
+	if _, err := d.GetQuery(expired, token.NoPos); err == nil {
+		panic("Expected GetQuery to fail with expired context")
+	}
+
+	if _, err := d.GetVersion(expired); err == nil {
+		panic("Expected GetVersion to fail with expired context")
+	}
+
+	if _, err := d.GetYamls(expired); err == nil {
+		panic("Expected GetYamls to fail with expired context")
+	}
+
+	if _, err := d.GetDiagnostics(expired); err == nil {
+		panic("Expected GetContent to fail with expired context")
+	}
+}
