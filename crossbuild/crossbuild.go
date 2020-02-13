ae58fb9252b65cac58077eff5988ffc613441451
Tobias Guggenmos <tguggenm@redhat.com>
2020-02-13 15:12:51 +0100

    Remove zip files from crossbuild
    Signed-off-by: Tobias Guggenmos <tguggenm@redhat.com>

--- crossbuild/crossbuild.go
+++ crossbuild/crossbuild.go
@@ -53,7 +53,7 @@ func main() {
 
 		outputDir := fmt.Sprint(".build/promql-langserver-", version, ".", target.os, "-", target.arch)
 		tarName := fmt.Sprint("promql-langserver-", version, ".", target.os, "-", target.arch, ".tar.gz")
-		zipName := fmt.Sprint("promql-langserver-", version, ".", target.os, "-", target.arch, ".zip")
+		//zipName := fmt.Sprint("promql-langserver-", version, ".", target.os, "-", target.arch, ".zip")
 
 		if err := exec.Command("rm", "-rf", outputDir).Run(); err != nil {
 			log.Fatal(err)
@@ -81,8 +81,11 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if err := exec.Command("zip", "-r", zipName, outputDir).Run(); err != nil {
-			log.Fatal(err)
-		}
+		// We don't need zip files at the moment.
+		/*
+			if err := exec.Command("zip", "-r", zipName, outputDir).Run(); err != nil {
+				log.Fatal(err)
+			}
+		*/
 	}
 }
