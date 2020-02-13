619214f3fe519a76528f00b1d8b84d22180afd85
Tobias Guggenmos <tguggenm@redhat.com>
2020-02-13 15:18:56 +0100

    Update repo path in vendoring mechanism
    Signed-off-by: Tobias Guggenmos <tguggenm@redhat.com>

--- vendored/go-tools.cmd
+++ vendored/go-tools.cmd
@@ -1,1 +1,1 @@
-sed --in-place -e 's/golang\.org\/x\/tools\/internal/github\.com\/slrtbtfs\/promql-lsp\/vendored\/go-tools/'
+sed --in-place -e 's/golang\.org\/x\/tools\/internal/github\.com\/prometheus-community\/promql-langserver\/vendored\/go-tools/'
