1e3cdaa69a18a757b6b14e3568984b3e17012ce4
Tobias Guggenmos <tguggenm@redhat.com>
2020-02-20 13:22:07 +0100

    Add a stub http handler func
    Signed-off-by: Tobias Guggenmos <tguggenm@redhat.com>

---
+++ stateless/handler.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Tobias Guggenmos
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stateless
+
+import "net/http"
+
+// Create an API handler for the stateless langserver API
+// Expects the URL of a Prometheus server as the argument.
+// Will fail if the Prometheus server is not reachable
+func CreateAPIHandler(prometheusURL string) http.Handler {
+	return http.NotFoundHandler()
+}
